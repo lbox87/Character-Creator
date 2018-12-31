e8aaf3626c34d9a877055c12842d753cf74a2cf5
Andrew Kolb <akolb87@gmail.com>
2018-12-30 20:26:22 -0600

    mock data visual update

--- public/mockdata.js
+++ public/mockdata.js
@@ -1,64 +1,30 @@
 // sample data template
-// {
-//     "status_updates": [
-//         {
-//             "id": "1111111",
-//             "text": "Can't believe how much fun I'm having.",
-//             "friendId": "aaaaaa",
-//             "friendName": "John Doe",
-//             "publishedAt": 1470016976609
-//         },
-//         {
-//             "id": "2222222",
-//             "text": "Have FOMO? Well you SHOULD!",
-//             "friendId": "bbbbbbb",
-//             "friendName": "Jane Doe",
-//             "publishedAt": 1470012976609
-//         },
-//         {
-//             "id": "333333",
-//             "text": "They're giving out immortality and free $$$ where I am.",
-//             "friendId": "cccc",
-//             "friendName": "Jim Doe",
-//             "publishedAt": 1470011976609
-//         },
-//         {
-//             "id": "4444444",
-//             "text": "humble brag humble brag humble brag",
-//             "friendId": "ddddd",
-//             "friendName": "Jackie Doe",
-//             "publishedAt": 1470009976609
-//         }
-//     ]
-// }
-
-
 var MOCK_STATUS_UPDATES = {
     "statusUpdates": [
         {
             "id": "1111111",
-            "text": "Can't believe how much fun I'm having.",
+            "text": "thing1",
             "friendId": "aaaaaa",
             "friendName": "John Doe",
             "publishedAt": 1470016976609
         },
         {
             "id": "2222222",
-            "text": "Have FOMO? Well you SHOULD!",
+            "text": "thing2",
             "friendId": "bbbbbbb",
             "friendName": "Jane Doe",
             "publishedAt": 1470012976609
         },
         {
             "id": "333333",
-            "text": "They're giving out immortality and free $$$ where I am.",
+            "text": "thing3",
             "friendId": "cccc",
             "friendName": "Jim Doe",
             "publishedAt": 1470011976609
         },
         {
             "id": "4444444",
-            "text": "humble brag humble brag humble brag",
+            "text": "thing4",
             "friendId": "ddddd",
             "friendName": "Jackie Doe",
             "publishedAt": 1470009976609
@@ -74,8 +40,8 @@ function getRecentStatusUpdates(callbackFn) {
 // to real API later
 function displayStatusUpdates(data) {
     for (index in data.statusUpdates) {
-       $('body').append(
-        '<p>' + data.statusUpdates[index].text + '</p>');
+       $('ul').append(
+        '<li>' + data.statusUpdates[index].text + '</li>');
     }
 }
 
