a185293c0232873ac5ee64211ec13ef499bd9fae
Andrew Kolb <akolb87@gmail.com>
2019-01-06 10:44:29 -0600

    testing attempts

--- test/test-http-200.js
+++ test/test-http-200.js
@@ -1,10 +1,26 @@
 const chai = require('chai');
-const expect = require('chai').expect;
+const chaiHttp = require('chai-http');
 
+// Import server.js and use destructuring assignment to create variables for
+// server.app, server.runServer, and server.closeServer
+const {app, runServer, closeServer} = require('../server');
+
+// declare a variable for expect from chai import
+const expect = chai.expect;
+
+chai.use(chaiHttp);
 
 chai.request('http://localhost:8080')
     .get('/')
-    .end(function (err, res) {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
+    // .end(function (err, res) {
+    //     expect(err).to.be.null;
+    //     expect(res).to.have.status(200);
+    // });
+
+
+    .then(function (res) {
+       expect(res).to.have.status(200);
+    })
+    .catch(function (err) {
+       throw err;
     });
