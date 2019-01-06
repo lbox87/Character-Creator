4f5feb8365676a1a23b47d70b9aeed7aaf5798d7
Andrew Kolb <akolb87@gmail.com>
2019-01-06 12:54:32 -0600

    testing attempts

--- test/test-http-200.js
+++ test/test-http-200.js
@@ -1,26 +1,35 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-// Import server.js and use destructuring assignment to create variables for
-// server.app, server.runServer, and server.closeServer
-const {app, runServer, closeServer} = require('../server');
-
-// declare a variable for expect from chai import
-const expect = chai.expect;
-
+const { app, runServer, closeServer } = require('../server');
+const expect = require('chai').expect;
 chai.use(chaiHttp);
 
-chai.request('http://localhost:8080')
-    .get('/')
-    // .end(function (err, res) {
-    //     expect(err).to.be.null;
-    //     expect(res).to.have.status(200);
-    // });
+const statusCheck = require('../server');
 
-
-    .then(function (res) {
-       expect(res).to.have.status(200);
-    })
-    .catch(function (err) {
-       throw err;
+describe('statusCheck', function () {
+    before(function () {
+        return runServer();
     });
+    after(function () {
+        return closeServer();
+    });
+
+    it('should respond a 200 HTTP status with intial page load', function () {
+
+        chai.request('http://localhost:8080')
+            .get('/')
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+            });
+
+        // .then(function (res) {
+        //    expect(res).to.have.status(200);
+        // })
+        // .catch(function (err) {
+        //    throw err;
+        // });
+     });
+    }
+);
