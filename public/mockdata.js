dff9c97f6679a184882753dba5e53d8eb0340858
Andrew Kolb <akolb87@gmail.com>
2019-01-09 19:30:59 -0600

    mock data update

--- public/mockdata.js
+++ public/mockdata.js
@@ -1,56 +1,62 @@
 // sample data template
-var MOCK_STATUS_UPDATES = {
-    "statusUpdates": [
+var currentCharacters = {
+    "characters": [
         {
-            "id": "1111111",
-            "text": "thing1",
-            "friendId": "aaaaaa",
-            "friendName": "John Doe",
-            "publishedAt": 1470016976609
+            "name": "Joe Smash",
+            "race": "Human",
+            "class": "Barbarian",
+            "level": 1,
+            "alignment": "chaotic neutral"
         },
         {
-            "id": "2222222",
-            "text": "thing2",
-            "friendId": "bbbbbbb",
-            "friendName": "Jane Doe",
-            "publishedAt": 1470012976609
+            "name": "Jane Slash",
+            "race": "Elf",
+            "class": "Rogue",
+            "level": 2,
+            "alignment": "lawful good"
         },
         {
-            "id": "333333",
-            "text": "thing3",
-            "friendId": "cccc",
-            "friendName": "Jim Doe",
-            "publishedAt": 1470011976609
+            "name": "Jim Splash",
+            "race": "Half-Orc",
+            "class": "Wizard",
+            "level": 3,
+            "alignment": "true neutral"
         },
         {
-            "id": "4444444",
-            "text": "thing4",
-            "friendId": "ddddd",
-            "friendName": "Jackie Doe",
-            "publishedAt": 1470009976609
+            "name": "Jen Stash",
+            "race": "Halfling",
+            "class": "Bard",
+            "level": 4,
+            "alignment": "chaotic good"
         }
     ]
 };
 // sample page load
-function getRecentStatusUpdates(callbackFn) {
-    setTimeout(function(){ callbackFn(MOCK_STATUS_UPDATES)}, 100);
+function getCharacters(callbackFn) {
+    setTimeout(function(){ callbackFn(currentCharacters)}, 100);
 }
 
 // this function stays the same when we connect
 // to real API later
-function displayStatusUpdates(data) {
-    for (index in data.statusUpdates) {
+function displayCharacters(data) {
+    for (index in data.characters) {
        $('ul').append(
-        '<li>' + data.statusUpdates[index].text + '</li>');
+        '<li>' + 
+        data.characters[index].name + " the level " +
+        data.characters[index].level + " " +
+        data.characters[index].alignment + " " +
+        data.characters[index].race + " " +
+        data.characters[index].class + " " +
+        '</li>');
     }
 }
 
 // this function can stay the same even when we
 // are connecting to real API
-function getAndDisplayStatusUpdates() {
-    getRecentStatusUpdates(displayStatusUpdates);
+function getAndDisplayCharacters() {
+    getCharacters(displayCharacters);
 }
 
 $(function() {
-    getAndDisplayStatusUpdates();
+    getAndDisplayCharacters();
 })
