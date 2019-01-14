e9e5beb91c9c6861252139d57245599cbc7bd45f
Andrew Kolb <akolb87@gmail.com>
2019-01-14 17:39:47 -0600

    character schema defined

--- models.js
+++ models.js
@@ -2,24 +2,13 @@
 
 const mongoose = require('mongoose');
 
-// this is our schema to represent a restaurant
-const restaurantSchema = mongoose.Schema({
+// this is our schema to represent a character
+const characterSchema = mongoose.Schema({
   name: {type: String, required: true},
-  borough: {type: String, required: true},
-  cuisine: {type: String, required: true},
-  address: {
-    building: String,
-    // coord will be an array of string values
-    coord: [String],
-    street: String,
-    zipcode: String
-  },
-  // grades will be an array of objects
-  grades: [{
-    date: Date,
-    grade: String,
-    score: Number
-  }]
+  race: {type: String, required: true},
+  class: {type: String, required: true},
+  level: {type: Number, required: true},
+  alignment: {type: String, required: true},
 });
 
 // *virtuals* (http://mongoosejs.com/docs/guide.html#virtuals)
@@ -27,32 +16,32 @@ const restaurantSchema = mongoose.Schema({
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
-restaurantSchema.virtual('addressString').get(function() {
-  return `${this.address.building} ${this.address.street}`.trim();});
+// characterSchema.virtual('addressString').get(function() {
+//   return `${this.address.building} ${this.address.street}`.trim();});
 
 // this virtual grabs the most recent grade for a restaurant.
-restaurantSchema.virtual('grade').get(function() {
-  const gradeObj = this.grades.sort((a, b) => {return b.date - a.date;})[0] || {};
-  return gradeObj.grade;
-});
+// characterSchema.virtual('grade').get(function() {
+//   const gradeObj = this.grades.sort((a, b) => {return b.date - a.date;})[0] || {};
+//   return gradeObj.grade;
+// });
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
 // exposes *some* of the fields we want from the underlying data
-restaurantSchema.methods.serialize = function() {
+// characterSchema.methods.serialize = function() {
 
-  return {
-    id: this._id,
-    name: this.name,
-    cuisine: this.cuisine,
-    borough: this.borough,
-    grade: this.grade,
-    address: this.addressString
-  };
-};
+//   return {
+//     id: this._id,
+//     name: this.name,
+//     cuisine: this.cuisine,
+//     borough: this.borough,
+//     grade: this.grade,
+//     address: this.addressString
+//   };
+// };
 
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Restaurant = mongoose.model('Character', characterSchema);
 
 module.exports = {Restaurant};
