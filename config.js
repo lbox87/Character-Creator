ac8493f4178ffc1b91600585638ab613d79799c9
Andrew Kolb <akolb87@gmail.com>
2019-01-14 20:43:40 -0600

    server link

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || '';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || '';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:Password1!@ds255784.mlab.com:55784/character-creator';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://admin:Password1!@ds255784.mlab.com:55784/character-creator';
 exports.PORT = process.env.PORT || 8080;
