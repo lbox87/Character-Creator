25709f236ee2bc6f729959ca3f2a82f9851f46b8
Andrew Kolb <akolb87@gmail.com>
2019-01-26 18:06:13 -0600

    cleanup

--- server.js
+++ server.js
@@ -153,35 +153,8 @@ if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
-
-
-// function runServer() {
-//   const port = process.env.PORT || 8080;
-//   return new Promise((resolve, reject) => {
-//     server = app.listen(port, () => {
-//       console.log(`Your app is listening on port ${port}`);
-//       resolve(server);
-//     })
-//     .on('error', err => {
-//       reject(err);
-//     });
-//   });
-// }
-
-// function closeServer() {
-//     return new Promise((resolve, reject) => {
-//         server.close(err => {
-//             if (err) reject(err)
-//             resolve(server)
-//         })
-//     })
-// }
-
-// if (require.main === module) {
-//   runServer().catch(err => console.error(err));
-// };
-
 // export defined variables and functions to be used elsewhere
 // this syntax is an object that actually returns:
 // {app: app, runServer: runServer, closeServer: closeServer}
 module.exports = { app, runServer, closeServer }
+module.exports = { }
--- test/test-get-characters.js
+++ test/test-get-characters.js
@@ -14,11 +14,7 @@ describe('getCharacters', function() {
   after(function() {
     return closeServer();
   });
-  // `chai.request.get` is an asynchronous operation. When
-  // using Mocha with async operations, we need to either
-  // return an ES6 promise or else pass a `done` callback to the
-  // test that we call at the end. We prefer the first approach, so
-  // we just return the chained `chai.request.get` object.
+  
   it('should have response with characters in a json', function() {
     return chai.request(app)
       .get('/')
